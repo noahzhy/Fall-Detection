626908ad1bab05d7759d45067c5d66ce096c9409
noahzhy <noahzhy@gmail.com>
2019-08-20 14:18:56 +0800

    doing work

--- RGB_difference_draw.py
+++ RGB_difference_draw.py
@@ -1,4 +1,7 @@
+import os
 import cv2
+import csv
+import pandas as pd
 # import kalman
 # import numpy as np
 # import minpy.numpy as np
@@ -9,15 +12,16 @@ ax = []                    # 定义一个 x 轴的空列表用来接收动态的
 ay = []                    # 定义一个 y 轴的空列表用来接收动态的数据
 rate_w_h = []
 rate_speed = []
-area_w_h = []
+area_motion = []
+factor = []
 plt.ion()                  # 开启一个画图的窗口
 
 # plt.clf()
 # count = 0
+cap = cv2.VideoCapture('video_06.avi')
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 
-
-
-cap = cv2.VideoCapture('video_02.avi')
 # cap.set(cv2.CAP_PROP_FPS, 30)
 # Check if camera opened successfully
 if (cap.isOpened() == False):
@@ -27,15 +31,14 @@ frameNum = 0
 x,y,w,h = 0, 0, 0, 0
 tempSpeed = [0.0, 0.0]
 currentSpeed = [0.0, 0.0]
-a = 0
-tempa = 0
-area = 0
+a, tempa, area = 0, 0, 0
 
 # Read until video is completed
-while (cap.isOpened()):
+while cap.isOpened():
     # Capture frame-by-frame
     ret, frame = cap.read()
-    frame = cv2.resize(frame, (320,240))
+    # if (cap.get())
+    # frame = cv2.resize(frame, (320,240))
     frameNum += 1
     if ret == True:
         tempframe = frame.copy()
@@ -68,7 +71,7 @@ while (cap.isOpened()):
                 continue
 
             if (frameNum % 2 == 0):
-                currentSpeed = [float(x+w/2), float(y+h/2)]
+                currentSpeed = [x+w/2, y+h/2]
                 a = ((currentSpeed[0]-tempSpeed[0])**2 + (currentSpeed[1]-tempSpeed[1])**2)**0.5
                 # print(a)
                 if (a > 30):
@@ -76,26 +79,27 @@ while (cap.isOpened()):
                 tempSpeed = currentSpeed
                 tempa = a
 
-            rate_speed.append(a* 3)
 
             plt.clf()
             plt.title('Fall Analysis')
             ax.append(frameNum)
-            ay.append(y+h/2)                  # y of mid-point
-            rate_w_h.append(float(w/h)*100)     # rate of w/h
-            area_w_h.append(area/100)
+            ay.append(y)                    # y of mid-point
+            area_motion.append(area/100)
+            rate_w_h.append((w/h)*100)     # rate of w/h
+            # rate_speed.append(a* 3)
+            # factor.append(a*area/100)
 
             plt.plot(ax, ay, color='blue', label='y of mid-point')
-            plt.plot(ax, rate_w_h, color='green', label='rate of w/h')
-            plt.plot(ax, rate_speed, color='red', label='rate of speed')
-            plt.plot(ax, area_w_h, color='orange', label='area of motion')
+            plt.plot(ax, area_motion, color='orange', label='area of motion')
+            plt.plot(ax, rate_w_h, color='green', label='w/h')
+            # plt.plot(ax, rate_speed, color='red', label='acc')
+            # plt.plot(ax,factor, color='skyblue', label='factor')
+
             plt.legend()                        # 显示图例
             plt.xlabel('Frames')
 
-
             # plt.draw()                        # for ubuntu
             plt.pause(0.01)                     # for windows
-
 
             # rectangle画出矩形，frame是原图，(x,y)是矩阵的左上点坐标，(x+w,y+h)是矩阵右下点坐标
             cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
@@ -112,6 +116,20 @@ while (cap.isOpened()):
     else:
         break
 
+data = {
+    'A': ay,
+    'B': area_motion,
+    'C': rate_w_h
+    # 'C': factor
+}
+
+
+
+df = pd.DataFrame(data)
+
+df.to_csv("test.csv", mode='w',index=False, header=['A','B','C'])
+print(df)
+# kalman.show_data(df)
 
 plt.pause(5)
 plt.ioff()
--- kalman.py
+++ kalman.py
@@ -1,79 +1,72 @@
 import cv2
-import minpy.numpy as np
+import numpy as np
+# import minpy.numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def kalman_filter(data):
-    kalman = cv2.KalmanFilter(6, 6)
-    kalman.measurementMatrix = np.array([[1, 0, 0, 0, 0, 0],
-                                         [0, 1, 0, 0, 0, 0],
-                                         [0, 0, 1, 0, 0, 0],
-                                         [0, 0, 0, 1, 0, 0],
-                                         [0, 0, 0, 0, 1, 0],
-                                         [0, 0, 0, 0, 0, 1]], np.float32)
-    kalman.transitionMatrix = np.array([[1, 0, 0, 0, 0, 0],
-                                         [0, 1, 0, 0, 0, 0],
-                                         [0, 0, 1, 0, 0, 0],
-                                         [0, 0, 0, 1, 0, 0],
-                                         [0, 0, 0, 0, 1, 0],
-                                         [0, 0, 0, 0, 0, 1]], np.float32)
-    kalman.processNoiseCov = np.array([[1, 0, 0, 0, 0, 0],
-                                       [0, 1, 0, 0, 0, 0],
-                                       [0, 0, 1, 0, 0, 0],
-                                       [0, 0, 0, 1, 0, 0],
-                                       [0, 0, 0, 0, 1, 0],
-                                       [0, 0, 0, 0, 0, 1]], np.float32) * 0.003
-    kalman.measurementNoiseCov = np.array([[1, 0, 0, 0, 0, 0],
-                                          [0, 1, 0, 0, 0, 0],
-                                          [0, 0, 1, 0, 0, 0],
-                                          [0, 0, 0, 1, 0, 0],
-                                          [0, 0, 0, 0, 1, 0],
-                                          [0, 0, 0, 0, 0, 1]], np.float32) * 1
+    kalman = cv2.KalmanFilter(3, 3)
+    kalman.measurementMatrix = np.array([[1, 0, 0],
+                                         [0, 1, 0],
+                                         [0, 0, 1]], np.float32)
+    kalman.transitionMatrix = np.array([[1, 0, 0],
+                                        [0, 1, 0],
+                                        [0, 0, 1]], np.float32)
+    kalman.processNoiseCov = np.array([[1, 0, 0],
+                                       [0, 1, 0],
+                                       [0, 0, 1]], np.float32) * 0.001
+    kalman.measurementNoiseCov = np.array([[1, 0, 0],
+                                           [0, 1, 0],
+                                           [0, 0, 1]], np.float32) * 0.008
 
-    row_num = data.acc_x.size
+    row_num = data.A.size
 
     for i in range(row_num):
-        correct = np.array(data.iloc[i, 0:6].values, np.float32).reshape([6, 1])
+        correct = np.array(data.iloc[i, 0:3].values, np.float32).reshape([3, 1])
         kalman.correct(correct)
         predict = kalman.predict()
         data.iloc[i, 0] = predict[0]
         data.iloc[i, 1] = predict[1]
         data.iloc[i, 2] = predict[2]
-        data.iloc[i, 3] = predict[3]
-        data.iloc[i, 4] = predict[4]
-        data.iloc[i, 5] = predict[5]
+        # data.iloc[i, 3] = predict[3]
 
     return data
 
 
 def show_data(data, name=None):
+    # data = pd.read_csv('test.csv')
     '''
     show data
     :param data: DataFrame
     :return:
     '''
-    num = data.acc_x.size
+    num = data.A.size
 
     x = np.arange(num)
-    fig = plt.figure(1, figsize=(100, 60))
+    fig = plt.figure(1, figsize=(50, 30))
     # 子表1绘制加速度传感器数据
     plt.subplot(2, 1, 1)
     plt.title('Origin')
-    plt.plot(x, data.acc_x, label='x')
-    plt.plot(x, data.acc_y, label='y')
-    plt.plot(x, data.acc_z, label='z')
+    plt.plot(x, data.A, label='A')
+    plt.plot(x, data.B, label='B')
+    plt.plot(x, data.C, label='C')
+    # plt.plot(x, data.D, label='D')
 
     # 添加解释图标
     plt.legend()
     x_flag = np.arange(0, num, num / 10)
     plt.xticks(x_flag)
 
+    tmp_data = kalman_filter(data)
+
     # 子表2绘制陀螺仪传感器数据
     plt.subplot(2, 1, 2)
     plt.title('After kalman')
-    plt.plot(x, data.gyro_x, label='x')
-    plt.plot(x, data.gyro_y, label='y')
-    plt.plot(x, data.gyro_z, label='z')
+    plt.plot(x, tmp_data.A, label='A')
+    plt.plot(x, tmp_data.B, label='B')
+    plt.plot(x, tmp_data.C, label='C')
+    plt.plot(x, (tmp_data.A*0.2+tmp_data.B*0.5+tmp_data.C*0.3), label='w+Avg')
 
     plt.legend()
     plt.xticks(x_flag)
@@ -83,3 +76,13 @@ def show_data(data, name=None):
     else:
         plt.savefig(name)
     plt.close()
+
+
+
+
+data = pd.read_csv('test.csv')
+# #
+# show_data(data)
+data = kalman_filter(data)
+# # data.to_csv('./dataset/train/BSC_1_1_annotated.csv', index=False)
+show_data(data)
